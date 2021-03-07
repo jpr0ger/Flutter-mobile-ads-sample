b659e004bb54f2b6e89961b56cd1bb9ecbdf76b5
jproger <jproger.dev@gmail.com>
2021-03-08 00:34:03 +0300

    Add simple list of data

---
+++ lib/data.dart
@@ -0,0 +1,22 @@
+const List<DataItem> dataList = [
+  DataItem('Item 1', 'Text for item 1'),
+  DataItem('Item 2', 'Text for item 2'),
+  DataItem('Item 3', 'Text for item 3'),
+  DataItem('Item 4', 'Text for item 4'),
+  DataItem('Item 5', 'Text for item 5'),
+  DataItem('Item 6', 'Text for item 6'),
+  DataItem('Item 7', 'Text for item 7'),
+  DataItem('Item 8', 'Text for item 8'),
+  DataItem('Item 9', 'Text for item 9'),
+  DataItem('Item 10', 'Text for item 10'),
+  DataItem('Item 11', 'Text for item 11'),
+  DataItem('Item 12', 'Text for item 12'),
+  DataItem('Item 13', 'Text for item 13'),
+];
+
+class DataItem {
+  final String title;
+  final String text;
+
+  const DataItem(this.title, this.text);
+}
